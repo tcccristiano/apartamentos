4aaf59cdd17222b32890d9456f06acdfe32b55fc
Kaio Cardamone <kaiocardamone@gmail.com>
2014-04-28 13:37:04 -0300

    modal eventos

--- eventos.php
+++ eventos.php
@@ -55,40 +55,48 @@ if($_SESSION['regra'] == 'admin'){
 
 <body>
 <div class="container">
-    <div id="newuserlivro">
-        <h4>Eventos</h4>
+
+    <a href="#modal" role="button" class="btn btn btn-success btn-large btn-block" style="margin:20px 0 40px; text-transform: uppercase; font-weight: bold;" data-toggle="modal">Criar Evento</a>
+
+    <!-- Modal -->
+    <div id="modal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+            <h3 id="myModalLabel">Eventos</h3>
+        </div>
+        <div class="modal-body">
+            <? if($_SESSION['regra'] == 'admin') {?>
+                <form name="cadastrodeusuarios" class="form-horizontal" method="post" action="" onsubmit="return validar(cadastrodeusuarios);">
+
+                    <div class="control-group">
+                        <label class="control-label" for="inputEmail">Título</label>
+                        <div class="controls">
+                            <input type="text" name="titulo" placeholder="Título" required>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <label class="control-label" for="inputEmail">Data do evento</label>
+                        <div class="controls">
+                            <input type="text" id="dtEvento" name="dtEvento" required>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="controls">
+                            <textarea style="width: 75%" class="form-control" rows="10" name="descricao" placeholder="Descreva o evento..." required ></textarea>
+                        </div>
+                    </div>
+                    <div class="control-group">
+                        <div class="controls">
+                            <button type="submit" name="submit" class="btn btn-primary">Enviar</button>
+                        </div>
+                    </div>
+                    <input type="hidden" name="data_criacao" value="<? echo date('Y-m-d H:m:s'); ?>">
+                    <input type="hidden" name="ativo" value="1">
+                </form>
+            <? } ?>
+        </div>
     </div>
-    <? if($_SESSION['regra'] == 'admin') {?>
-        <form name="cadastrodeusuarios" class="form-horizontal" method="post" action="" onsubmit="return validar(cadastrodeusuarios);">
-
-            <div class="control-group">
-                <label class="control-label" for="inputEmail">Título</label>
-                <div class="controls">
-                    <input type="text" name="titulo" placeholder="Título" required>
-                </div>
-            </div>
-            <div class="control-group">
-                <label class="control-label" for="inputEmail">Data do evento</label>
-                <div class="controls">
-                    <input type="text" id="dtEvento" name="dtEvento" required>
-                </div>
-            </div>
-            <div class="control-group">
-                <div class="controls">
-                    <textarea style="width: 75%" class="form-control" rows="10" name="descricao" placeholder="Descreva o evento..." required ></textarea>
-                </div>
-            </div>
-            <div class="control-group">
-                <div class="controls">
-                    <button type="submit" name="submit" class="btn btn-primary"">Enviar</button>
-                </div>
-            </div>
-            <input type="hidden" name="data_criacao" value="<? echo date('Y-m-d H:m:s'); ?>">
-            <input type="hidden" name="ativo" value="1">
-        </form>
-    <? } ?>
     <? foreach($listarEventos as $listarEvento){ ?>
-
         <table class="table table-bordered">
             <thead>
             <tr>
